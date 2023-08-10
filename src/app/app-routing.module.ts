de3d22380b62763b66e6731150ba9b5cbba4e977
The Meeper <lou@dvbytsolutions.com>
2023-08-10 21:04:06 +0200

    added azure login to my system

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UnauthComponent } from './unauth/unauth.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { AuthenticationGuard } from 'microsoft-adal-angular6';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent,canActivate: [AuthenticationGuard] },
+  { path: 'unauth', component: UnauthComponent },
+  
+  { path: '',   redirectTo: '/home', pathMatch: 'full'}, // redirect to   // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
