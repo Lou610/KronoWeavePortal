2e7129ae78bf1eecd96a85cbdc618ce0befe9953
TheMeeper <lou@dvbytsolutions.com>
2023-08-21 22:10:57 +0200

    Added API calls
    Added API calls

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,13 @@ import { HomeComponent } from './home/home.component';
 import { UnauthComponent } from './unauth/unauth.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-import { MsAdalAngular6Module,AuthenticationGuard } from 'microsoft-adal-angular6';
+import {
+  MsAdalAngular6Module,
+  AuthenticationGuard,
+} from 'microsoft-adal-angular6';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,24 +21,26 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     HomeComponent,
     UnauthComponent,
     PagenotfoundComponent,
-    SidebarComponent
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     MsAdalAngular6Module.forRoot({
       tenant: '4072b378-679d-4c55-91d7-ba82ed371ebc',
       clientId: 'f75d91b2-fd57-4db4-a5ba-a443e499ffd5',
       redirectUri: 'http://localhost:4200/home',
-      endpoints: { 
-      'api application url': 'api application client id', // this is for feteching the access token
+      endpoints: {
+        'api application url': 'api application client id', // this is for feteching the access token
       },
       navigateToLoginRequestUrl: false,
       cacheLocation: '<localStorage /sessionStorage>',
-      postLogoutRedirectUri: 'URI on which you want to redirect user after logout',
-      }),
+      postLogoutRedirectUri:
+        'URI on which you want to redirect user after logout',
+    }),
   ],
   providers: [AuthenticationGuard],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild } from '@angular/core';
 import { MsAdalAngular6Service } from 'microsoft-adal-angular6';
+import { HttpService } from './../http.service';
+import { HttpClient } from '@angular/common/http';
 
 function extractUsernameFromEmail(email: string): string | null {
   const parts = email.split('@');
@@ -8,6 +10,18 @@ function extractUsernameFromEmail(email: string): string | null {
     return parts[0];
   }
   return null;
+}
+
+export interface User {
+  EmailAddress: string;
+  userName: string;
+  License: string;
+}
+
+export interface License {
+  Id: string;
+  license1: string;
+  UserId: string;
 }
 
 @Component({
@@ -19,17 +33,48 @@ export class HomeComponent implements OnInit {
   Email: string;
   UserName: any;
   License: any;
-  constructor(private adalService: MsAdalAngular6Service) {
+  Paid: string;
+  Free: string;
+
+  constructor(
+    private adalService: MsAdalAngular6Service,
+    public service: HttpService,
+    public http: HttpClient
+  ) {
+    this.Paid = 'Paid';
+    this.Free = 'Free';
+
     if (this.adalService.userInfo) {
       this.Email = this.adalService.userInfo.userName;
-      this.UserName = extractUsernameFromEmail(this.Email);
-      this.License = 'Pro';
+
+      this.GetUserName();
+
+      this.GetLicense();
     } else {
       console.log('User information not available.');
     }
   }
 
   ngOnInit(): void {}
+
+  GetUserName() {
+    this.http
+      .get<User>('https://localhost:7151/api/GetUserName?Email=' + this.Email)
+      .subscribe((data) => {
+        this.UserName = data.userName;
+      });
+  }
+
+  GetLicense() {
+    this.http
+      .get<License>(
+        'https://localhost:7151/api/GetLicenseType?Email=' + this.Email
+      )
+      .subscribe((data) => {
+        console.log('License ' + JSON.stringify(data));
+        this.License = data.license1;
+      });
+  }
 
   login(): void {
     this.adalService.login();
